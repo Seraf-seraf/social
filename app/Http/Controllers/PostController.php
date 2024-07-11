f9326a0e224fc3e4ed54779b5f888e59d4138b33
radekkrab <radxxx7@mail.ru>
2024-07-11 13:07:03 +0500

    add Post Store

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Post\StoreRequest;
+use App\Http\Resources\Post\PostResource;
+use App\Models\Post;
+use App\Models\PostImage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
@@ -25,11 +30,38 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreRequest $request)
     {
-        //
+        $data = $request->validated();
+        
+        try {
+            DB::beginTransaction();
+        
+            $imageId = $data['image_id'];
+            unset($data['image_id']);
+
+            $data['user_id'] = auth()->id();
+            $post = Post::create($data);
+
+            $this->proccessImage($post, $imageId);
+
+            DB::commit();
+            } catch (\Exception $exception) {
+                DB::rollBack();
+                return response()->json(['error' => $exception->getMessage()]);
+            }
+
+            return new PostResource($post);
     }
 
+    private function proccessImage($post, $imageId) {
+        if (isset($imageId)) {
+            $image = PostImage::find($imageId);
+            $image->update([
+                'post_id' => $post->id
+            ]);
+        }
+    }
     /**
      * Display the specified resource.
      */
--- resources/js/views/user/Personal.vue
+++ resources/js/views/user/Personal.vue
@@ -109,7 +109,7 @@ export default {
             const formData = new FormData()
             formData.append('image', file)
 
-            axios.post('/api/post_image', formData)
+            axios.post('/api/post_images', formData)
                 .then(res => {
                     this.image = res.data.data
                 })
